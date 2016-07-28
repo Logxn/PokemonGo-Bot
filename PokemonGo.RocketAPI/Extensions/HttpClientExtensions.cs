030ce1bfa3a69cdf076e691e5b5a10441e862c7a
Ar1u <mroario@t-online.de>
2016-07-29 00:39:36 +0200

    Fix Errors

--- PokemonGo.RocketAPI/Extensions/HttpClientExtensions.cs
+++ PokemonGo.RocketAPI/Extensions/HttpClientExtensions.cs
@@ -16,8 +16,9 @@ namespace PokemonGo.RocketAPI.Extensions
         public static async Task<TResponsePayload> PostProtoPayload<TRequest, TResponsePayload>(this HttpClient client, string url, TRequest request) where TRequest : IMessage<TRequest> where TResponsePayload : IMessage<TResponsePayload>, new()
         {
             Logger.Write($"Requesting {typeof(TResponsePayload).Name}", LogLevel.Debug);
+            System.Threading.Thread.Sleep(200);
             var response = await PostProto<TRequest>(client, url, request);
-
+             
             if (response.Payload.Count == 0)
                 throw new InvalidResponseException();
 
