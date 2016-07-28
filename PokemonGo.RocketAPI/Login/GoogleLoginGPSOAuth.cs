344079d792f968dc560a4b63036e337a17ea1075
Ar1u <mroario@t-online.de>
2016-07-28 19:43:01 +0200

    Google Login 2fa

--- PokemonGo.RocketAPI/Login/GoogleLoginGPSOAuth.cs
+++ PokemonGo.RocketAPI/Login/GoogleLoginGPSOAuth.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using DankMemes.GPSOAuthSharp;
 using Newtonsoft.Json;
 using PokemonGo.RocketAPI.Exceptions;
+using System.Diagnostics;
+using System.Threading;
 
 namespace PokemonGo.RocketAPI.Login
 {
@@ -15,6 +17,18 @@ namespace PokemonGo.RocketAPI.Login
         {
             GPSOAuthClient client = new GPSOAuthClient(username, password);
             Dictionary<string, string> response = client.PerformMasterLogin();
+
+            if (response.ContainsValue("NeedsBrowser"))
+            {
+                Logger.Error("Your Google Account uses 2FA. Create a Password for the Application here:");
+                Logger.Error("https://security.google.com/settings/security/apppasswords");
+                Logger.Error("And use that for Login with Google.");
+                Logger.Error("Opening the Site in 5 Seconds.");
+                Thread.Sleep(5000);
+                Process.Start("https://security.google.com/settings/security/apppasswords");
+                Logger.Error("The Program is now freezed.");
+                Thread.Sleep(50000000);
+            }
 
             if (response.ContainsKey("Error"))
                 throw new GoogleException(response["Error"]);
