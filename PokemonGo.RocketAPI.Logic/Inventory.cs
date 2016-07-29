5e09566d2161ea4b3914ab719493e8a704be792f
Ar1u <mroario@t-online.de>
2016-07-29 04:32:39 +0200

    Excel list of Pokemons every restart #152

--- PokemonGo.RocketAPI.Logic/Inventory.cs
+++ PokemonGo.RocketAPI.Logic/Inventory.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using PokemonGo.RocketAPI.GeneratedCode;
 using System;
 using System.Threading;
+using System.IO;
 
 namespace PokemonGo.RocketAPI.Logic
 {
@@ -42,6 +43,13 @@ namespace PokemonGo.RocketAPI.Logic
             var p = await GetItems();
             i = p.Count();
             return i;
+        }
+
+        public async Task<IEnumerable<PokemonData>> GetHighestsPerfect(int limit = 1000)
+        {
+            var myPokemon = await GetPokemons();
+            var pokemons = myPokemon.ToList();
+            return pokemons.OrderByDescending(PokemonInfo.CalculatePokemonPerfection).Take(limit);
         }
 
         public async Task<IEnumerable<PokemonData>> GetPokemons()
@@ -155,6 +163,76 @@ namespace PokemonGo.RocketAPI.Logic
                 .Where(x => x.Count() > 1)
                 .SelectMany(p => p.Where(x => x.Favorite == 0 && PokemonInfo.CalculatePokemonPerfection(x) <= _client.getSettingHandle().ivmaxpercent).OrderByDescending(x => x.Cp).ThenBy(n => n.StaminaMax).Skip(_client.getSettingHandle().HoldMaxDoublePokemons).ToList());
         }
+
+        public async Task ExportPokemonToCSV(Profile player, string filename = "PokemonList.csv")
+        {
+            if (player == null)
+                return;
+            var stats = await GetPlayerStats();
+            var stat = stats.FirstOrDefault();
+            if (stat == null)
+                return;
+             
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Configs"); 
+
+            if (Directory.Exists(path))
+            {
+                try
+                {
+                    string pokelist_file = Path.Combine(path, $"Profile_{player.Username}_{filename}");
+                    if (File.Exists(pokelist_file))
+                        File.Delete(pokelist_file);
+                    string ls = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ListSeparator;
+                    string header = "PokemonID,Name,NickName,CP / MaxCP,IV Perfection in %,Attack 1,Attack 2,HP,Attk,Def,Stamina,Familie Candies,IsInGym,IsFavorite,previewLink";
+                    File.WriteAllText(pokelist_file, $"{header.Replace(",", $"{ls}")}");
+
+                    var AllPokemon = await GetHighestsPerfect();
+                    var myPokemonSettings = await GetPokemonSettings();
+                    var pokemonSettings = myPokemonSettings.ToList();
+                    var myPokemonFamilies = await GetPokemonFamilies();
+                    var pokemonFamilies = myPokemonFamilies.ToArray();
+                    int trainerLevel = stat.Level;
+                    int[] exp_req = new[] { 0, 1000, 3000, 6000, 10000, 15000, 21000, 28000, 36000, 45000, 55000, 65000, 75000, 85000, 100000, 120000, 140000, 160000, 185000, 210000, 260000, 335000, 435000, 560000, 710000, 900000, 1100000, 1350000, 1650000, 2000000, 2500000, 3000000, 3750000, 4750000, 6000000, 7500000, 9500000, 12000000, 15000000, 20000000 };
+                    int exp_req_at_level = exp_req[stat.Level - 1];
+
+                    using (var w = File.AppendText(pokelist_file))
+                    {
+                        w.WriteLine("");
+                        foreach (var pokemon in AllPokemon)
+                        {
+                            string toEncode = $"{(int)pokemon.PokemonId}" + "," + trainerLevel + "," + PokemonInfo.GetLevel(pokemon) + "," + pokemon.Cp + "," + pokemon.Stamina;
+                            //Generate base64 code to make it viewable here http://poke.isitin.org/#MTUwLDIzLDE3LDE5MDIsMTE4
+                            var encoded = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(toEncode));
+
+                            string IsInGym = string.Empty;
+                            string IsFavorite = string.Empty;
+ 
+                            if (pokemon.Favorite != 0)
+                                IsFavorite = "Yes";
+                            else
+                                IsFavorite = "No";
+
+                            var settings = pokemonSettings.Single(x => x.PokemonId == pokemon.PokemonId);
+                            var familiecandies = pokemonFamilies.Single(x => settings.FamilyId == x.FamilyId).Candy;
+                            string perfection = PokemonInfo.CalculatePokemonPerfection(pokemon).ToString("0.00");
+                            perfection = perfection.Replace(",", ls == "," ? "." : ",");
+                            string content_part1 = $"{(int)pokemon.PokemonId},{pokemon.PokemonId},{pokemon.Nickname},{pokemon.Cp}/{PokemonInfo.CalculateMaxCP(pokemon)},";
+                            string content_part2 = $",{pokemon.Move1},{pokemon.Move2},{pokemon.Stamina},{pokemon.IndividualAttack},{pokemon.IndividualDefense},{pokemon.IndividualStamina},{familiecandies},{IsInGym},{IsFavorite},http://poke.isitin.org/#{encoded}";
+                            string content = $"{content_part1.Replace(",", $"{ls}")}\"{perfection}\"{content_part2.Replace(",", $"{ls}")}";
+                            w.WriteLine($"{content}");
+
+                        }
+                        w.Close();
+                    }
+                    Logger.ColoredConsoleWrite(ConsoleColor.Green, $"Export Player Infos and all Pokemon to \"\\Export\\{filename}\"", LogLevel.Info);
+                }
+                catch
+                {
+                    Logger.Error("Export Player Infos and all Pokemons to CSV not possible. File seems be in use!", LogLevel.Warning);
+                }
+            }
+        }
+
 
         public async Task<IEnumerable<Item>> GetItems()
         {
