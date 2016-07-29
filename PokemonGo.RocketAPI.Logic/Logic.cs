9e2228daa8e1c3695533d58c2c536e804cdec507
Ar1u <mroario@t-online.de>
2016-07-29 19:14:38 +0200

    Fixed

--- PokemonGo.RocketAPI.Logic/Logic.cs
+++ PokemonGo.RocketAPI.Logic/Logic.cs
@@ -293,25 +293,22 @@ namespace PokemonGo.RocketAPI.Logic
                     }
 
                     Logger.ColoredConsoleWrite(ConsoleColor.Green, $"Farmed XP: {fortSearch.ExperienceAwarded}, Gems: { fortSearch.GemsAwarded}, Eggs: {egg} Items: {i}", LogLevel.Info);
-                } else
-                {
-                    if (fortSearch.CooldownCompleteTimestampMs != 0)
+                } else if (fortInfo.Name != null) {
+                    failed_softban++;
+
+                    if (failed_softban >= 6)
                     {
-                        failed_softban++;
-                        if (failed_softban >= 6)
+                        Logger.Error("Detected a Softban. Trying to use our Special 1337 Unban Methode.");
+                        for (int i = 0; i < 60; i++)
                         {
-                            Logger.Error("Detected a Softban. Trying to use our Special 1337 Unban Methode.");
-                            for (int i = 0; i < 60; i++)
+                            var unban = await client.SearchFort(pokeStop.Id, pokeStop.Latitude, pokeStop.Longitude);
+                            if (unban.ExperienceAwarded > 0)
                             {
-                                var unban = await client.SearchFort(pokeStop.Id, pokeStop.Latitude, pokeStop.Longitude);
-                                if (unban.ExperienceAwarded > 0)
-                                {
-                                    break;
-                                }
+                                break;
                             }
-                            failed_softban = 0;
-                            Logger.ColoredConsoleWrite(ConsoleColor.Green, "Probably unbanned you.");
                         }
+                        failed_softban = 0;
+                        Logger.ColoredConsoleWrite(ConsoleColor.Green, "Probably unbanned you.");
                     }
                 }
 
