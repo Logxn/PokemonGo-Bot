97a215c32d9c18168da7073d17e6f29da164e4bc
globsch <globsch.globsch@gmail.com>
2016-07-29 13:07:03 +0200

    Update Logic.cs
    keepPokemonsThatCanEvolve was not possible anymore.

--- PokemonGo.RocketAPI.Logic/Logic.cs
+++ PokemonGo.RocketAPI.Logic/Logic.cs
@@ -468,7 +468,7 @@ namespace PokemonGo.RocketAPI.Logic
         {
             if (_clientSettings.TransferDoublePokemons)
             {
-                var duplicatePokemons = await _inventory.GetDuplicatePokemonToTransfer();
+                var duplicatePokemons = await _inventory.GetDuplicatePokemonToTransfer(keepPokemonsThatCanEvolve);
 
                 foreach (var duplicatePokemon in duplicatePokemons)
                 {
