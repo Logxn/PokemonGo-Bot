05bb700dcd3bcea3f8091bf5e100dd87a3e0eb74
FrancYescO <francyesc0@hotmail.it>
2016-07-31 19:30:42 +0200

    Fix Evolvable Check + Unified Candy column

--- PokemonGo.RocketAPI.Console/Pokemons.designer.cs
+++ PokemonGo.RocketAPI.Console/Pokemons.designer.cs
@@ -59,6 +59,7 @@
             this.listView1.Size = new System.Drawing.Size(687, 387);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
             // 
             // button1
