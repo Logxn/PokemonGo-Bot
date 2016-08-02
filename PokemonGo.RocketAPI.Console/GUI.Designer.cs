4395f1e6b134180d1d3b6591c20515e4aea56f92
setoy <setoy@my-horizon.de>
2016-08-02 21:39:31 +0200

    Disabled UI resizing
    No point in allowing resize when all it does is creating more gray
    space...

--- PokemonGo.RocketAPI.Console/GUI.Designer.cs
+++ PokemonGo.RocketAPI.Console/GUI.Designer.cs
@@ -1201,6 +1201,7 @@
             this.Controls.Add(this.label32);
             this.Controls.Add(this.label30);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "GUI";
