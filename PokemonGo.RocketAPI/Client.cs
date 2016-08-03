9339be2515dba12e69915fdc7ccfba4856dbc668
Ar1u <mroario@t-online.de>
2016-08-04 00:10:38 +0200

    update hash

--- PokemonGo.RocketAPI/Client.cs
+++ PokemonGo.RocketAPI/Client.cs
@@ -336,7 +336,7 @@ namespace PokemonGo.RocketAPI
                     Message =
                         new Request.Types.SettingsGuid
                         {
-                            Guid = ByteString.CopyFromUtf8("4a2e9bc330dae60e7b74fc85b98868ab4700802e")
+                            Guid = ByteString.CopyFromUtf8("b1f2bf509a025b7cd76e1c484e2a24411c50f061")
                         }.ToByteString()
                 });
 
