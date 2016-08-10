1194cafc5a286e52d42a2900a6140cc2493d0d8d
Ar1u <mroario@t-online.de>
2016-08-10 16:39:39 +0200

    fix crashing at start

--- PokemonGo.RocketAPI.Console/Properties/AssemblyInfo.cs
+++ PokemonGo.RocketAPI.Console/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("1fea147e-f705-467b-a538-00b053b5f819")]
 
-[assembly: AssemblyVersion("1.0.8.7")]
-[assembly: AssemblyFileVersion("1.0.8.7")]
+[assembly: AssemblyVersion("1.0.8.8")]
+[assembly: AssemblyFileVersion("1.0.8.8")]
