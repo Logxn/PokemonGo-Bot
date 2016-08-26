80d64137da971fc52c32f21b0c1f6fa6b8facb18
Ar1u <mroario@t-online.de>
2016-08-26 20:31:06 +0200

    new build

--- PokemonGo.RocketAPI.Console/Properties/AssemblyInfo.cs
+++ PokemonGo.RocketAPI.Console/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("1fea147e-f705-467b-a538-00b053b5f819")]
 
-[assembly: AssemblyVersion("1.1.0.6")]
-[assembly: AssemblyFileVersion("1.1.0.6")]
+[assembly: AssemblyVersion("1.1.0.7")]
+[assembly: AssemblyFileVersion("1.1.0.7")]
