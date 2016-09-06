ecb38230a1683ece0ce44cdce7d53895fa74c1a0
Xelwon <xelwonpgo@gmail.com>
2016-09-06 03:01:53 +0200

    changed CP perfection with IV perfection
    changed CP perfection with IV perfection and added new column at the end with CP perfection

--- PokemonGo.RocketAPI.Console/Pokemons.cs
+++ PokemonGo.RocketAPI.Console/Pokemons.cs
@@ -143,6 +143,9 @@ namespace PokemonGo.RocketAPI.Console
                     columnheader = new ColumnHeader();
                     columnheader.Text = "#";
                     PokemonListView.Columns.Add(columnheader);
+                    columnheader = new ColumnHeader();
+                    columnheader.Text = "% CP";
+                    PokemonListView.Columns.Add(columnheader);
 
                     foreach (var pokemon in pokemons)
                     {
@@ -164,7 +167,7 @@ namespace PokemonGo.RocketAPI.Console
                             .Select(f => f.Candy_)
                             .First();
                         listViewItem.SubItems.Add(string.Format("{0}", pokemon.Cp));
-                        listViewItem.SubItems.Add(string.Format("{0}% {1}-{2}-{3}", PokemonInfo.CalculatePokemonPerfection(pokemon).ToString("0.00"), pokemon.IndividualAttack, pokemon.IndividualDefense, pokemon.IndividualStamina));
+                        listViewItem.SubItems.Add(string.Format("{0}% {1}-{2}-{3}", PokemonInfo.CalculatePokemonPerfectionIV(pokemon).ToString("0.00"), pokemon.IndividualAttack, pokemon.IndividualDefense, pokemon.IndividualStamina));
                         listViewItem.SubItems.Add(string.Format("{0}", PokemonInfo.GetLevel(pokemon)));
                         listViewItem.ImageKey = pokemon.PokemonId.ToString();
 
@@ -195,6 +198,7 @@ namespace PokemonGo.RocketAPI.Console
                         listViewItem.SubItems.Add(string.Format("{0}", pokemon.Move1));
                         listViewItem.SubItems.Add(string.Format("{0} ({1})", pokemon.Move2, PokemonInfo.GetAttack(pokemon.Move2)));
                         listViewItem.SubItems.Add(string.Format("{0}", (int) pokemon.PokemonId));
+                        listViewItem.SubItems.Add(string.Format("{0}", PokemonInfo.CalculatePokemonPerfection(pokemon).ToString("0.00")));
 
                         PokemonListView.Items.Add(listViewItem);
                     }
