2217da9b5d06909252edc4b25b08dc2aee3ec370
Logxn <logxncs@gmail.com>
2016-09-08 17:55:13 +0200

    uff fix doch mal alter

--- PokemonGo.RocketAPI.Console/Location.cs
+++ PokemonGo.RocketAPI.Console/Location.cs
@@ -23,10 +23,6 @@ namespace PokemonGo.RocketAPI.Console
 
         private GMarkerGoogle _botMarker = new GMarkerGoogle(new PointLatLng(),  Properties.Resources.player);
 
-
-        private GMarkerGoogle _botMarker = new GMarkerGoogle(new PointLatLng(), GMarkerGoogleType.red_small);
-        //private GMarkerGoogle _botMarker = new GMarkerGoogle(new PointLatLng(),  Properties.red_small);
-
         private GMapRoute _botRoute = new GMapRoute("BotRoute");
         private GMapOverlay _pokeStopsOverlay = new GMapOverlay("PokeStops");
         private Dictionary<string, GMarkerGoogle> _pokeStopsMarks = new Dictionary<string, GMarkerGoogle>();
