12d13af50dafc481843588c329387ceade92fb64
Gabriel Kalani <gabrielsilva1956@gmail.com>
2016-09-12 15:23:50 -0300

    CODE COMMIT 3815 - Beautified

--- PokemonGo.RocketAPI.Logic/LogicInfoObservable.cs
+++ PokemonGo.RocketAPI.Logic/LogicInfoObservable.cs
@@ -6,77 +6,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PokemonGo.RocketAPI.Logic
-{
-    /// <summary>
-    /// "Observable" mades live infos possible
-    /// </summary>
-    public class LogicInfoObservable
-    {
-        /// <summary>
-        /// GeoLocations
-        /// </summary>
-        public delegate void GeoLocationHandler(GeoCoordinate value);
-        public event GeoLocationHandler HandleNewGeoLocations = delegate { };
-        public void PushNewGeoLocations(GeoCoordinate newValue)
-        {
-            HandleNewGeoLocations(newValue);
-        }
+namespace PokemonGo.RocketAPI.Logic {
+ /// <summary>
+ /// "Observable" mades live infos possible
+ /// </summary>
+ public class LogicInfoObservable {
+  /// <summary>
+  /// GeoLocations
+  /// </summary>
+  public delegate void GeoLocationHandler(GeoCoordinate value);
+  public event GeoLocationHandler HandleNewGeoLocations = delegate {};
+  public void PushNewGeoLocations(GeoCoordinate newValue) {
+   HandleNewGeoLocations(newValue);
+  }
 
-                /// <summary>
-        /// GeoLocations
-        /// </summary>
-        public delegate void NewPokemonLocationHandler(List<PokemonGo.RocketApi.PokeMap.DataCollector.PokemonMapData> mapData);
-        public event NewPokemonLocationHandler HandleNewPokemonLocations = delegate { };
-        public void PushNewPokemonLocations(List<PokemonGo.RocketApi.PokeMap.DataCollector.PokemonMapData> mapData)
-        {
-            HandleNewPokemonLocations(mapData);
-        }
+  /// <summary>
+  /// GeoLocations
+  /// </summary>
+  public delegate void NewPokemonLocationHandler(List < PokemonGo.RocketApi.PokeMap.DataCollector.PokemonMapData > mapData);
+  public event NewPokemonLocationHandler HandleNewPokemonLocations = delegate {};
+  public void PushNewPokemonLocations(List < PokemonGo.RocketApi.PokeMap.DataCollector.PokemonMapData > mapData) {
+   HandleNewPokemonLocations(mapData);
+  }
 
-        public delegate void ClearPokemonsHandler();
-        public event ClearPokemonsHandler HandleClearPokemon = delegate { };
-        public void PushClearPokemons()
-        {
- 
-                HandleClearPokemon();
-        }
+  public delegate void ClearPokemonsHandler();
+  public event ClearPokemonsHandler HandleClearPokemon = delegate {};
+  public void PushClearPokemons() {
 
-        public delegate void AvailablePokeStopHandler(FortData[] pokeStops);
-        public event AvailablePokeStopHandler HandleAvailablePokeStop = delegate { };
-        public void PushAvailablePokeStopLocations(FortData[] pokeStop)
-        {
-            if (pokeStop != null && pokeStop.Any())
-            {
-                HandleAvailablePokeStop(pokeStop);
-            }
-        }
+   HandleClearPokemon();
+  }
 
-        public delegate void AvailablePokeGymHandler(FortData[] pokeGyms);
-        public event AvailablePokeGymHandler HandleAvailablePokeGym = delegate { };
-        public void PushAvailablePokeGymsLocations(FortData[] pokeGyms)
-        {
-            if (pokeGyms != null && pokeGyms.Any())
-            {
-                HandleAvailablePokeGym(pokeGyms);
-            }
-        }
+  public delegate void AvailablePokeStopHandler(FortData[] pokeStops);
+  public event AvailablePokeStopHandler HandleAvailablePokeStop = delegate {};
+  public void PushAvailablePokeStopLocations(FortData[] pokeStop) {
+   if (pokeStop != null && pokeStop.Any()) {
+    HandleAvailablePokeStop(pokeStop);
+   }
+  }
 
-        public delegate void PokeStopInfoUpdateHandler(FortData pokeStop, string info);
-        public event PokeStopInfoUpdateHandler HandlePokeStopInfoUpdate = delegate { };
-        public void PushPokeStopInfoUpdate(FortData pokeStop, string info)
-        {
-            HandlePokeStopInfoUpdate(pokeStop, info);
-        }
+  public delegate void AvailablePokeGymHandler(FortData[] pokeGyms);
+  public event AvailablePokeGymHandler HandleAvailablePokeGym = delegate {};
+  public void PushAvailablePokeGymsLocations(FortData[] pokeGyms) {
+   if (pokeGyms != null && pokeGyms.Any()) {
+    HandleAvailablePokeGym(pokeGyms);
+   }
+  }
 
-        /// <summary>
-        /// Hunting stats
-        /// </summary>
-        public delegate void HuntStatsHandler(string value);
-        public event HuntStatsHandler HandleNewHuntStats = delegate { };
-        public void PushNewHuntStats(string newValue)
-        {
-            HandleNewHuntStats(newValue);
-        }
-    }
+  public delegate void PokeStopInfoUpdateHandler(FortData pokeStop, string info);
+  public event PokeStopInfoUpdateHandler HandlePokeStopInfoUpdate = delegate {};
+  public void PushPokeStopInfoUpdate(FortData pokeStop, string info) {
+   HandlePokeStopInfoUpdate(pokeStop, info);
+  }
+
+  /// <summary>
+  /// Hunting stats
+  /// </summary>
+  public delegate void HuntStatsHandler(string value);
+  public event HuntStatsHandler HandleNewHuntStats = delegate {};
+  public void PushNewHuntStats(string newValue) {
+   HandleNewHuntStats(newValue);
+  }
+ }
 
 }
