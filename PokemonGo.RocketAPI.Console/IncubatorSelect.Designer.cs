00042b072b9ad42d36a3d71aabcc03c7ef340825
Logxn <logxncs@gmail.com>
2016-09-24 19:28:29 +0200

    Normal Titles!

--- PokemonGo.RocketAPI.Console/IncubatorSelect.Designer.cs
+++ PokemonGo.RocketAPI.Console/IncubatorSelect.Designer.cs
@@ -46,112 +46,114 @@ namespace PokemonGo.RocketAPI.Console
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IncubatorSelect));
-			this.buttonCancel = new System.Windows.Forms.Button();
-			this.listView = new System.Windows.Forms.ListView();
-			this.chID = new System.Windows.Forms.ColumnHeader();
-			this.chItemID = new System.Windows.Forms.ColumnHeader();
-			this.chUsesRemaining = new System.Windows.Forms.ColumnHeader();
-			this.chIncubatorType = new System.Windows.Forms.ColumnHeader();
-			this.chStartKmWalked = new System.Windows.Forms.ColumnHeader();
-			this.chTargetKmWalked = new System.Windows.Forms.ColumnHeader();
-			this.chPokemon = new System.Windows.Forms.ColumnHeader();
-			this.buttonOk = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// buttonCancel
-			// 
-			this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.buttonCancel.Location = new System.Drawing.Point(596, 282);
-			this.buttonCancel.Margin = new System.Windows.Forms.Padding(2);
-			this.buttonCancel.Name = "buttonCancel";
-			this.buttonCancel.Size = new System.Drawing.Size(55, 23);
-			this.buttonCancel.TabIndex = 83;
-			this.buttonCancel.Text = "Cancel";
-			this.buttonCancel.UseVisualStyleBackColor = true;
-			// 
-			// listView
-			// 
-			this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-			| System.Windows.Forms.AnchorStyles.Left) 
-			| System.Windows.Forms.AnchorStyles.Right)));
-			this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-			this.chID,
-			this.chItemID,
-			this.chUsesRemaining,
-			this.chIncubatorType,
-			this.chStartKmWalked,
-			this.chTargetKmWalked,
-			this.chPokemon});
-			this.listView.FullRowSelect = true;
-			this.listView.GridLines = true;
-			this.listView.Location = new System.Drawing.Point(12, 12);
-			this.listView.Name = "listView";
-			this.listView.Size = new System.Drawing.Size(639, 265);
-			this.listView.TabIndex = 82;
-			this.listView.UseCompatibleStateImageBehavior = false;
-			this.listView.View = System.Windows.Forms.View.Details;
-			// 
-			// chID
-			// 
-			this.chID.Text = "ID";
-			// 
-			// chItemID
-			// 
-			this.chItemID.Text = "Item ID";
-			this.chItemID.Width = 100;
-			// 
-			// chUsesRemaining
-			// 
-			this.chUsesRemaining.Text = "Uses Remaining";
-			this.chUsesRemaining.Width = 100;
-			// 
-			// chIncubatorType
-			// 
-			this.chIncubatorType.Text = "Incubator Type";
-			this.chIncubatorType.Width = 100;
-			// 
-			// chStartKmWalked
-			// 
-			this.chStartKmWalked.Text = "Start Km Walked";
-			this.chStartKmWalked.Width = 100;
-			// 
-			// chTargetKmWalked
-			// 
-			this.chTargetKmWalked.Text = "Target Km Walked";
-			this.chTargetKmWalked.Width = 100;
-			// 
-			// chPokemon
-			// 
-			this.chPokemon.Text = "Pokemon";
-			this.chPokemon.Width = 100;
-			// 
-			// buttonOk
-			// 
-			this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.buttonOk.Location = new System.Drawing.Point(537, 282);
-			this.buttonOk.Margin = new System.Windows.Forms.Padding(2);
-			this.buttonOk.Name = "buttonOk";
-			this.buttonOk.Size = new System.Drawing.Size(55, 23);
-			this.buttonOk.TabIndex = 84;
-			this.buttonOk.Text = "Ok";
-			this.buttonOk.UseVisualStyleBackColor = true;
-			this.buttonOk.Click += new System.EventHandler(this.ButtonOkClick);
-			// 
-			// IncubatorSelect
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.CancelButton = this.buttonCancel;
-			this.ClientSize = new System.Drawing.Size(663, 316);
-			this.Controls.Add(this.buttonOk);
-			this.Controls.Add(this.buttonCancel);
-			this.Controls.Add(this.listView);
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Name = "IncubatorSelect";
-			this.Text = "Select Incubator";
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IncubatorSelect));
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.chID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chItemID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chUsesRemaining = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chIncubatorType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chStartKmWalked = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chTargetKmWalked = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chPokemon = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(795, 347);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(73, 28);
+            this.buttonCancel.TabIndex = 83;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chID,
+            this.chItemID,
+            this.chUsesRemaining,
+            this.chIncubatorType,
+            this.chStartKmWalked,
+            this.chTargetKmWalked,
+            this.chPokemon});
+            this.listView.FullRowSelect = true;
+            this.listView.GridLines = true;
+            this.listView.Location = new System.Drawing.Point(16, 15);
+            this.listView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(851, 325);
+            this.listView.TabIndex = 82;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // chID
+            // 
+            this.chID.Text = "ID";
+            // 
+            // chItemID
+            // 
+            this.chItemID.Text = "Item ID";
+            this.chItemID.Width = 100;
+            // 
+            // chUsesRemaining
+            // 
+            this.chUsesRemaining.Text = "Uses Remaining";
+            this.chUsesRemaining.Width = 137;
+            // 
+            // chIncubatorType
+            // 
+            this.chIncubatorType.Text = "Incubator Type";
+            this.chIncubatorType.Width = 121;
+            // 
+            // chStartKmWalked
+            // 
+            this.chStartKmWalked.Text = "Start Km Walked";
+            this.chStartKmWalked.Width = 116;
+            // 
+            // chTargetKmWalked
+            // 
+            this.chTargetKmWalked.Text = "Target Km Walked";
+            this.chTargetKmWalked.Width = 136;
+            // 
+            // chPokemon
+            // 
+            this.chPokemon.Text = "Pokemon";
+            this.chPokemon.Width = 100;
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOk.Location = new System.Drawing.Point(716, 347);
+            this.buttonOk.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(73, 28);
+            this.buttonOk.TabIndex = 84;
+            this.buttonOk.Text = "Ok";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.ButtonOkClick);
+            // 
+            // IncubatorSelect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(884, 389);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.listView);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "IncubatorSelect";
+            this.Text = "PokemonGoBot | Incubator Selection";
+            this.ResumeLayout(false);
 
 		}
 	}
--- PokemonGo.RocketAPI.Console/Update.Designer.cs
+++ PokemonGo.RocketAPI.Console/Update.Designer.cs
@@ -36,44 +36,48 @@
             // 
             // progress
             // 
-            this.progress.Location = new System.Drawing.Point(12, 12);
+            this.progress.Location = new System.Drawing.Point(16, 15);
+            this.progress.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.progress.Name = "progress";
-            this.progress.Size = new System.Drawing.Size(516, 23);
+            this.progress.Size = new System.Drawing.Size(687, 28);
             this.progress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
             this.progress.TabIndex = 0;
             // 
             // status
             // 
             this.status.Enabled = false;
-            this.status.Location = new System.Drawing.Point(12, 42);
+            this.status.Location = new System.Drawing.Point(16, 52);
+            this.status.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.status.Name = "status";
             this.status.ReadOnly = true;
-            this.status.Size = new System.Drawing.Size(516, 20);
+            this.status.Size = new System.Drawing.Size(687, 22);
             this.status.TabIndex = 1;
             this.status.Text = "Status...";
             // 
             // comp
             // 
             this.comp.AutoSize = true;
-            this.comp.Location = new System.Drawing.Point(9, 70);
+            this.comp.Location = new System.Drawing.Point(12, 86);
+            this.comp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.comp.Name = "comp";
-            this.comp.Size = new System.Drawing.Size(63, 13);
+            this.comp.Size = new System.Drawing.Size(83, 17);
             this.comp.TabIndex = 2;
             this.comp.Text = "Completed: ";
             // 
             // Update
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(540, 92);
+            this.ClientSize = new System.Drawing.Size(720, 113);
             this.Controls.Add(this.comp);
             this.Controls.Add(this.status);
             this.Controls.Add(this.progress);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Update";
-            this.Text = "Update";
+            this.Text = "PokemonGoBot | Update System";
             this.Load += new System.EventHandler(this.Update_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
