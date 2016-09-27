1ca3dc7ef9517abd813c9135b644d35a4e3f7695
MorganHuangPhx <Morgan_Huang@Phoenix.com>
2016-09-27 16:18:13 +0800

    Add files via upload
    1. Added language translation for Chinese.
    2. Added the field of Device ID setting for UI.

--- PokemonGo.RocketAPI.Console/Properties/Resources.Designer.cs
+++ PokemonGo.RocketAPI.Console/Properties/Resources.Designer.cs
@@ -183,6 +183,16 @@ namespace PokemonGo.RocketAPI.Console.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap taiwan {
+            get {
+                object obj = ResourceManager.GetObject("taiwan", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap team_instinct {
             get {
                 object obj = ResourceManager.GetObject("team_instinct", resourceCulture);
