0d1ba5d1547c874cafdb8fa6bb4cb40e11c9f828
Logxn <logxncs@gmail.com>
2016-11-19 10:37:55 +0100

    Eggs can now be prioritized + Typo Fix
    * You can now prioritize Eggs
    * Typo Fix
    * Build Update

--- PokemonGo.RocketAPI.Console/Properties/AssemblyInfo.cs
+++ PokemonGo.RocketAPI.Console/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("1fea147e-f705-467b-3872-00d053b5f818")]
 
 
-[assembly: AssemblyVersion("1.1.9.0")]
-[assembly: AssemblyFileVersion("1.1.9.0")]
+[assembly: AssemblyVersion("1.1.9.1")]
+[assembly: AssemblyFileVersion("1.1.9.1")]
