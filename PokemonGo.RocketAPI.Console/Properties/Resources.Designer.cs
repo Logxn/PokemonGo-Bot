457d61ae5f1457f0a0424e35e99b934d2966b860
LordBunny <querijnrotmg@gmail.com>
2017-01-19 18:45:51 +0100

    Added Theme

--- PokemonGo.RocketAPI.Console/Properties/Resources.Designer.cs
+++ PokemonGo.RocketAPI.Console/Properties/Resources.Designer.cs
@@ -143,6 +143,16 @@ namespace PokemonGo.RocketAPI.Console.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap image1 {
+            get {
+                object obj = ResourceManager.GetObject("image1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap map {
             get {
                 object obj = ResourceManager.GetObject("map", resourceCulture);
