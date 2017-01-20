c9783e3a9334d91f72c63032e7d54359ba529a1e
Eduard Biete <esbiete@gmail.com>
2017-01-20 19:16:14 +0100

    v1.2.0.6
    IMPORTANT: Bot is still not safe due to unknown changes in API
    
    - Fix : AppVersion to 5301
    +Add: appart from checking versions, we also check kill switch at start
    * Improvement: DeviceInfo moved out of Helper to DeviceSetup.cs to clean RequestBuilder.cs
    * Improvement: changes message for kill switch

---
+++ PokemonGo.RocketAPI/Helpers/DeviceSetup.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static POGOProtos.Networking.Envelopes.Signature.Types;
+
+namespace PokemonGo.RocketAPI.Helpers
+{
+    class DeviceSetup
+    {
+        // Reads last setup device (a file generated on last run)
+        public static string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Device");
+        public static string deviceinfo = Path.Combine(path, "DeviceInfo.txt");
+
+        public DeviceInfo setUpDevice()
+        {
+            DeviceInfo _device = new DeviceInfo();
+
+            string[] arrLine = File.ReadAllLines(deviceinfo);
+
+            string DevicePackageName = arrLine[0].ToString();
+            // Read DeviceID of File
+            if (arrLine[1].ToString() != " ")
+            {
+                _device.DeviceId = arrLine[1].ToString();
+            }
+            else
+            {
+                _device.DeviceId = GenerateRandomDeviceId();
+                // Save to file
+                string[] b = { DevicePackageName, _device.DeviceId };
+
+                File.WriteAllLines(deviceinfo, b);
+            }
+
+            // Setuprest
+            _device.AndroidBoardName = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["AndroidBoardName"];
+            _device.AndroidBootloader = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["AndroidBootloader"];
+            _device.DeviceBrand = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["DeviceBrand"];
+            _device.DeviceModel = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["DeviceModel"];
+            _device.DeviceModelBoot = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["DeviceModelBoot"];
+            _device.DeviceModelIdentifier = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["DeviceModelIdentifier"];
+            _device.FirmwareBrand = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["FirmwareBrand"];
+            _device.FirmwareFingerprint = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["FirmwareFingerprint"];
+            _device.FirmwareTags = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["FirmwareTags"];
+            _device.FirmwareType = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["FirmwareType"];
+            _device.HardwareManufacturer = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["HardwareManufacturer"];
+            _device.HardwareModel = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["HardwareModel"];
+
+            return _device;
+            //setupdevicedone = true;
+        }
+
+        public static string BytesToHex(byte[] bytes)
+        {
+            char[] hexArray = "0123456789abcdef".ToCharArray();
+            char[] hexChars = new char[bytes.Length * 2];
+            for (int index = 0; index < bytes.Length; index++)
+            {
+                int var = bytes[index] & 0xFF;
+                hexChars[index * 2] = hexArray[(int)((uint)var >> 4)];
+                hexChars[index * 2 + 1] = hexArray[var & 0x0F];
+            }
+            return new string(hexChars).ToLower();
+        }
+
+        private static string GenerateRandomDeviceId(long numBytes = 16)
+        {
+            var bytes = new byte[numBytes];
+            new Random().NextBytes(bytes);
+            return BytesToHex(bytes);
+        }
+
+        public static DeviceInfo GetRandomIosDevice()
+        {
+            DeviceInfo deviceInfo = new DeviceInfo();
+
+            // iOS device id (UDID) are 16 bytes long.
+            var bytes = new byte[16];
+            new Random().NextBytes(bytes);
+            var deviceId = BytesToHex(bytes);
+
+            deviceInfo.DeviceId = deviceId;
+            deviceInfo.FirmwareType = DeviceInfoHelper.IosVersions[new Random().Next(DeviceInfoHelper.IosVersions.Length)];
+            string[] device = DeviceInfoHelper.IosDeviceInfo[new Random().Next(DeviceInfoHelper.IosDeviceInfo.Length)];
+            deviceInfo.DeviceModelBoot = device[0];
+            deviceInfo.DeviceModel = device[1];
+
+            deviceInfo.HardwareModel = device[2];
+            deviceInfo.FirmwareBrand = "iPhone OS";
+            deviceInfo.DeviceBrand = "Apple";
+            deviceInfo.HardwareManufacturer = "Apple";
+
+            return deviceInfo;
+        }
+
+    }
+}
--- PokemonGo.RocketAPI/Helpers/RequestBuilder.cs
+++ PokemonGo.RocketAPI/Helpers/RequestBuilder.cs
@@ -31,79 +31,18 @@ namespace PokemonGo.RocketAPI.Helpers
         private readonly Client _client;
         private readonly float _speed;
         private ByteString _sessionHash;
-
-
-        /// Device Shit
-        public static string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Device");
-        public static string deviceinfo = Path.Combine(path, "DeviceInfo.txt");
-
-        public string DeviceId;
-        public string AndroidBoardName;
-        public string AndroidBootloader;
-        public string DeviceBrand;
-        public string DeviceModel;
-        public string DeviceModelIdentifier;
-        public string DeviceModelBoot;
-        public string HardwareManufacturer;
-        public string HardwareModel;
-        public string FirmwareBrand;
-        public string FirmwareTags;
-        public string FirmwareType;
-        public string FirmwareFingerprint;
-
+        public DeviceInfo _DeviceInfo;
+        
         private int _token2 = RandomDevice.Next(1, 59);
-
         public byte[] sessionhash_array = null;
-
-
-
         public bool setupdevicedone = false;
-
-        public void setUpDevice()
+      
+        public RequestBuilder(Client client, string authToken, AuthType authType, double latitude, double longitude, double altitude, AuthTicket authTicket = null)
         {
-            string[] arrLine = File.ReadAllLines(deviceinfo);
-
-            string DevicePackageName = arrLine[0].ToString();
-            // Read DeviceID of File
-            if (arrLine[1].ToString() != " ")
-            {
-                DeviceId = arrLine[1].ToString();
-            }
-            else
-            {
-                DeviceId = RandomString(16, "0123456789abcdef");
-                // Save to file
-                string[] b = { DevicePackageName, DeviceId };
-
-                File.WriteAllLines(deviceinfo, b);
-            }
-
-            // Setuprest
-            AndroidBoardName = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["AndroidBoardName"];
-            AndroidBootloader = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["AndroidBootloader"];
-            DeviceBrand = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["DeviceBrand"];
-            DeviceModel = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["DeviceModel"];
-            DeviceModelBoot = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["DeviceModelBoot"];
-            DeviceModelIdentifier = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["DeviceModelIdentifier"];
-            FirmwareBrand = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["FirmwareBrand"];
-            FirmwareFingerprint = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["FirmwareFingerprint"];
-            FirmwareTags = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["FirmwareTags"];
-            FirmwareType = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["FirmwareType"];
-            HardwareManufacturer = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["HardwareManufacturer"];
-            HardwareModel = DeviceInfoHelper.DeviceInfoSets[DevicePackageName]["HardwareModel"];
-
-            setupdevicedone = true;
-        }
-
-
-        public RequestBuilder(Client client, string authToken, AuthType authType, 
-                              double latitude, double longitude, double altitude,
-                                AuthTicket authTicket = null)
-        {
-
             if (!setupdevicedone)
             {
-                setUpDevice();
+                _DeviceInfo = new DeviceSetup().setUpDevice() ;// setUpDevice();
+                setupdevicedone = true;
             }
 
             _client = client;
@@ -114,21 +53,15 @@ namespace PokemonGo.RocketAPI.Helpers
             _altitude = altitude;
             _authTicket = authTicket;
 
-
             // Add small variance to speed.
             _speed = _speed + ((float)Math.Round(GenRandom(-1, 1), 7));
-
-
 
             if (_sessionHash == null)
             {
                 _sessionHash = GenerateNewHash();
             }
-
-
         }
-
-
+        
         public ByteString GenerateNewHash()
         {
             var hashBytes = new byte[16];
@@ -197,43 +130,11 @@ namespace PokemonGo.RocketAPI.Helpers
         /// internal async Task<PlatformRequest> GenerateSignatureAsync(RequestEnvelope requestEnvelope)
         private RequestEnvelope.Types.PlatformRequest GenerateSignature(RequestEnvelope requestEnvelope)
         {
-
             var timestampSinceStart = (long)(Utils.GetTime(true) - _client.StartTime);
             var locationFixes = BuildLocationFixes(requestEnvelope, timestampSinceStart);
 
             requestEnvelope.Accuracy = locationFixes[0].Altitude;
             requestEnvelope.MsSinceLastLocationfix = (long)locationFixes[0].TimestampSnapshot;
-
-            //// ???
-            //byte[] ticketBytes = _authTicket != null ? _authTicket.ToByteArray() : Encoding.UTF8.GetBytes(_authToken);
-
-            //// ???
-            //if (sessionhash_array == null)
-            //{
-            //    byte[] rByte = new byte[16];
-            //    Random ra = new Random();
-            //    ra.NextBytes(rByte);
-            //    sessionhash_array = rByte;
-            //}
-
-            #region -- DeviceInfo
-            DeviceInfo dInfo = new DeviceInfo
-            {
-                DeviceId = this.DeviceId,
-                AndroidBoardName = this.AndroidBoardName, // might al
-                AndroidBootloader = this.AndroidBootloader,
-                DeviceBrand = this.DeviceBrand,
-                DeviceModel = this.DeviceModel, // might als
-                DeviceModelIdentifier = this.DeviceModelIdentifier,
-                DeviceModelBoot = this.DeviceModelBoot,
-                HardwareManufacturer = this.HardwareManufacturer,
-                HardwareModel = this.HardwareModel,
-                FirmwareBrand = this.FirmwareBrand,
-                FirmwareTags = this.FirmwareTags,
-                FirmwareType = this.FirmwareType,
-                FirmwareFingerprint = this.FirmwareFingerprint
-            };
-            #endregion
 
             #region GenerateSignature
             var signature = new Signature
@@ -267,7 +168,7 @@ namespace PokemonGo.RocketAPI.Helpers
                         Status = 3
                     }
                 },
-                DeviceInfo = dInfo,
+                DeviceInfo = _DeviceInfo,// dInfo,
                 LocationFix = { locationFixes },
                 ActivityStatus = new ActivityStatus
                 {
@@ -287,8 +188,6 @@ namespace PokemonGo.RocketAPI.Helpers
 
             var requestsBytes = requestEnvelope.Requests.Select(x => x.ToByteArray()).ToArray();
 
-            //string envelopString = JsonConvert.SerializeObject(requestEnvelope);
-
             HashRequestContent hashRequest = new HashRequestContent()
             {
                 Timestamp = signature.Timestamp,
@@ -300,19 +199,12 @@ namespace PokemonGo.RocketAPI.Helpers
                 Requests = new List<byte[]>(requestsBytes)                
             };
 
-            //var hashRequestContent = new StringContent(JsonConvert.SerializeObject(hashRequest), Encoding.UTF8, "application/json"); <- Done by PokeHashHasher
-
             HashResponseContent responseContent;
 
             responseContent = _client.Hasher.RequestHashes(hashRequest);
 
             signature.LocationHash1 = unchecked((int)responseContent.LocationAuthHash);
             signature.LocationHash2 = unchecked((int)responseContent.LocationHash);
-
-            //foreach (var item in res.RequestHashes)
-            //{
-            //    signature.RequestHash.Add((unchecked((ulong)item)));
-            //}
             signature.RequestHash.AddRange(responseContent.RequestHashes.Select(x => (ulong) x).ToArray());
 
             var encryptedSignature = new PlatformRequest
@@ -358,13 +250,10 @@ namespace PokemonGo.RocketAPI.Helpers
                 locationFixes.Insert(0, new LocationFix
                 {
                     TimestampSnapshot = (ulong)timestampSnapshot,
-                    //Latitude = LocationUtil.OffsetLatitudeLongitude(_latitude, Random.Next(100) + 10),
                     Latitude = (float)_client.CurrentLatitude,
-                    //Longitude = LocationUtil.OffsetLatitudeLongitude(_longitude, Random.Next(100) + 10),
                     Longitude = (float)_client.CurrentLongitude,
                     HorizontalAccuracy = (float)Random.NextDouble(5.0, 25.0),
                     VerticalAccuracy = (float)Random.NextDouble(5.0, 25.0),
-                    //Altitude = (float)Random.NextDouble(10.0, 30.0),
                     Altitude = (float)_client.CurrentAltitude,
                     Provider = "fused",
                     ProviderStatus = 3,
@@ -373,18 +262,12 @@ namespace PokemonGo.RocketAPI.Helpers
                     Course = -1,
                     // Floor = 0
                 });
-                //if (_settings.EnableVerboseLogging)
-                //{
-                //    Logger.ColoredConsoleWrite(ConsoleColor.Blue, $"[DEBUG] Request: {requestEnvelope.Requests[0].RequestType} LF:({locationFixes[0].Latitude},{locationFixes[0].Longitude},{locationFixes[0].Altitude}) CL:({_client.CurrentLatitude},{_client.CurrentLongitude},{_client.CurrentAltitude})");
-                //}
             }
-
             return locationFixes;
         }
 
         internal class LocationUtil
         {
-
             public static float OffsetLatitudeLongitude(double lat, double ran)
             {
                 const int round = 6378137;
@@ -392,7 +275,6 @@ namespace PokemonGo.RocketAPI.Helpers
 
                 return (float)(lat + dl * 180 / Math.PI);
             }
-
         }
 
         public async Task<RequestEnvelope> GetRequestEnvelope(Request[] customRequests, bool firstRequest = false)
@@ -457,30 +339,6 @@ namespace PokemonGo.RocketAPI.Helpers
         {
             Random r = new Random();
             return r.NextDouble() * (max - min) + min;
-        }
-
-        private string RandomString(int length, string alphabet = "abcdefghijklmnopqrstuvwxyz0123456789")
-        {
-            var outOfRange = Byte.MaxValue + 1 - (Byte.MaxValue + 1) % alphabet.Length;
-
-            return string.Concat(
-                Enumerable
-                    .Repeat(0, Int32.MaxValue)
-                    .Select(e => this.RandomByte())
-                    .Where(randomByte => randomByte < outOfRange)
-                    .Take(length)
-                    .Select(randomByte => alphabet[randomByte % alphabet.Length])
-            );
-        }
-
-        private byte RandomByte()
-        {
-            using (var randomizationProvider = new RNGCryptoServiceProvider())
-            {
-                var randomBytes = new byte[1];
-                randomizationProvider.GetBytes(randomBytes);
-                return randomBytes.Single();
-            }
         }
     }
 }
