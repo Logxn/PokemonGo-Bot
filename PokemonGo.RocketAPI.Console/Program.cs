8ae78cdd62a9eee9ea85dbdf3607d2061ba2d787
esbiete <esbiete@gmail.com>
2017-01-20 15:00:43 +0100

    Preliminary changes for 0.53.0 update (#4209)
    * UNK25 Update and new PCryptPokeHash
    
    * Fix RequestBuilder for v51
    
    * Some clean up
    
    * * Solved problems with execution contexts in all modules
    * Removed some async methods in UI
    
    Know Problems:
    - Pokemon List actions do not work
    
    * PokemonsPanel changes
    
    * * Solved GUI interaction problems by converting all async GUI tasks to sync
    * Correct altitude if <= 0
    * sync with upstream
    
    * Change of httpClientExtensions.cs for proper upstream sync
    
    * Corrected elevation in LocationPanel
    
    * Using correct altitude
    
    * Test to improve Pokemon catch
    
    * +Fix: added EndOfLine at Evolve log output
    
    * + Implementation of cached GetMap
    + start of Inventory.cs organization
    * Commit of version 1.1.9.8
    
    * Inventory & Map cleaning (on-going)
    
    * PROTOS update
    
    * * [Xelwon] Fixed Egg incubation (problem with json file
    * Fix GetInventory cache calls
    + Added Bulk Transfer pokemon
    
    * * Inventory.cs clean
    * Map.cs clean
    
    * + Fixing some try&catch
    
    * * LocationPanel - clean header
    * ChangesPanel - clean for including new vars
    
    * Few merge corrections
    
    * Some corrections for last sync
    
    * v1.2.0.4
    
    * Check Current NIANTIC API version
    * Close Bot in case it is not supported
    * Added command line help
    
    * Littel update of CurrentAPIVersion.cs (does not affect exe)
    
    * * Arrangements for v0.53.0

--- PokemonGo.RocketAPI.Console/Program.cs
+++ PokemonGo.RocketAPI.Console/Program.cs
@@ -108,28 +108,29 @@ namespace PokemonGo.RocketAPI.Console
             // Checking if current BOT API implementation supports NIANTIC current API (unless there's an override command line switch)
             if (!GlobalSettings.BypassCheckCompatibilityVersion)
             {
-                var NianticAPIVersion = new CurrentAPIVersion().GetCurrentAPIVersion();
                 Logger.ColoredConsoleWrite(ConsoleColor.DarkMagenta, $"Bot Current version: {BotVersion}");
                 Logger.ColoredConsoleWrite(ConsoleColor.DarkMagenta, $"Bot Supported API version: {GlobalSettings.BotApiSupportedVersion}");
-                Logger.ColoredConsoleWrite(ConsoleColor.DarkMagenta, $"Current API version: {NianticAPIVersion}");
-                bool CurrentVersionsOK = new CurrentAPIVersion().CheckAPIVersionCompatibility( GlobalSettings.BotApiSupportedVersion, new Version(NianticAPIVersion));
+                Logger.ColoredConsoleWrite(ConsoleColor.DarkMagenta, $"Current API version: {new CurrentAPIVersion().GetNianticAPIVersion()}");
+                bool CurrentVersionsOK = new CurrentAPIVersion().CheckAPIVersionCompatibility( GlobalSettings.BotApiSupportedVersion);
                 if (!CurrentVersionsOK)
                 {
                     Logger.ColoredConsoleWrite(ConsoleColor.Red, $"Atention, current API version is new and still not supported by Bot.");
                     Logger.ColoredConsoleWrite(ConsoleColor.Red, $"Bot will now exit to keep your account safe.");
                     Logger.ColoredConsoleWrite(ConsoleColor.Red, $"---------- PRESS ANY KEY TO CLOSE ----------");
+
                     System.Console.ReadKey();
                     Environment.Exit(-1);
                 }
             }
 
             var openGUI = false;
+
             if (args != null && args.Length > 0 && args[0].Contains("-nogui"))
             {
                 Logger.ColoredConsoleWrite(ConsoleColor.Red, "You added -nogui!");
 
                 if (!GlobalSettings.Load()) {
-                    Logger.ColoredConsoleWrite(ConsoleColor.Red, "You didnt setup correctly with the GUI.");
+                    Logger.ColoredConsoleWrite(ConsoleColor.Red, "You didn't setup correctly with the GUI.");
                     Logger.ColoredConsoleWrite(ConsoleColor.Red, "Run it without -nogui to Configure.");
                     Logger.ColoredConsoleWrite(ConsoleColor.Red, "Exiting..");
                     Environment.Exit(-1);
