537219259a6752f16087f4506c2abe8bbc5baf38
Xelwon <xelwonpgo@gmail.com>
2017-01-24 00:04:24 +0100

    New Translation System (#4220)
    * UNK25 Update and new PCryptPokeHash
    
    * Fix RequestBuilder for v51
    
    * Some clean up
    
    * * Solved problems with execution contexts in all modules
    * Removed some async methods in UI
    
    Know Problems:
    - Pokemon List actions do not work
    
    * PokemonsPanel changes
    
    * * Solved GUI interaction problems by converting all async GUI tasks to sync
    * Correct altitude if <= 0
    * sync with upstream
    
    * Change of httpClientExtensions.cs for proper upstream sync
    
    * Corrected elevation in LocationPanel
    
    * Using correct altitude
    
    * Test to improve Pokemon catch
    
    * +Fix: added EndOfLine at Evolve log output
    
    * + Implementation of cached GetMap
    + start of Inventory.cs organization
    * Commit of version 1.1.9.8
    
    * binary update to earlier stable version.
    
    * update pogoprotos files.
    
    * Inventory & Map cleaning (on-going)
    
    * PROTOS update
    
    * Deleted old pogoprotos files and added new files to the project.
    
    * Updates in rocketapi
    
    * update problem in logic of eggs
    
    * fixed problem in repeared pokemons
    
    * Changing sniping
    
    * spine with 3 tries to find nearby pokemons
    
    * added number of tries in sniper
    
    * * [Xelwon] Fixed Egg incubation (problem with json file
    * Fix GetInventory cache calls
    + Added Bulk Transfer pokemon
    
    * trying to solve many tries in sniping
    
    * fixing manual snipe
    
    * * Inventory.cs clean
    * Map.cs clean
    
    * + Fixing some try&catch
    
    * Added Panel for Tutorial
    
    * * LocationPanel - clean header
    * ChangesPanel - clean for including new vars
    
    * Few merge corrections
    
    * manual snipe problems solved
    
    * fixed problem in manual sniping used to latest merge
    
    * Some corrections for last sync
    
    * rebuilt "manual snipe" logic
    
    * Small fixes
    
    * Increased default seconds to wait on manual snipe
    
    * Binary updated.
    
    * added tutorial settings
    
    * Fix catch pokemon when it is disabled
    
    * Binary update
    
    * binary update.
    
    * version changed to prevent infinite downloading.
    
    * test increasing hash request for 150 RPM
    
    * third try
    
    * fix in key queue
    
    * fix sniping when throw exceptions in its logic.
    
    * changes in remove from queue
    
    * added dialog to configure Avatar preferences.
    added web browser to ease access to sniping pokemon pages.
    
    * changed method of share information between projects
    
    * rewritten method to share information.
    
    * Performance Changes in Configuration Window
    
    * Internationalization Test
    
    * v1.2.0.4
    
    * Check Current NIANTIC API version
    * Close Bot in case it is not supported
    * Added command line help
    
    * Littel update of CurrentAPIVersion.cs (does not affect exe)
    
    * changing tests to ease translation
    
    * added esbiete changes
    
    * renamed some forgotten variables
    
    * fixed logic of esbiete to stop bot when is not same api version.
    
    * Deleted blocks and repeats in pokemons list.
    
    * several changes in webpanel
    
    * binary update
    
    * added PlatformRequestType.UnknownPrt8
    
    * changed place to add "UnknownPrt8"
    
    * Updated to "alucas 250 branch" pogoprotos
    
    * binary update
    
    * fix load settings.
    
    * added metro framework libraries
    
    * Changes in GUI settings
    
    * bugfix changing budy pokemon
    
    * Changed method to save profiles
    
    * Changed method to store profiles
    
    * fix encryption password option
    
    * added region
    
    * Added comment in request builder.
    
    * New translation system a new translator app to help with translations
    
    * file with strings to translate GUI

---
+++ PokemonGo.RocketAPI.Translator/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Xelwon
+ * Date: 23/01/2017
+ * Time: 20:51
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace PokemonGo.RocketAPI.Translator
+{
+    /// <summary>
+    /// Class with program entry point.
+    /// </summary>
+    internal sealed class Program
+    {
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+        
+    }
+}
