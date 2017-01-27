0d61aedc67a3c2494c5d52984b4477fb3e9b254a
git <xelwonpgo@gmail.com>
2017-01-27 22:54:22 +0100

    Fixed problems using web panel in manual sniping.

--- PokemonGo.RocketAPI.Console/Panels/WebPanel.Designer.cs
+++ PokemonGo.RocketAPI.Console/Panels/WebPanel.Designer.cs
@@ -39,53 +39,54 @@ namespace PokemonGo.RocketAPI.Console.Panels
         /// </summary>
         private void InitializeComponent()
         {
-        	this.webBrowser1 = new System.Windows.Forms.WebBrowser();
-        	this.textBox1 = new System.Windows.Forms.TextBox();
-        	this.button1 = new System.Windows.Forms.Button();
-        	this.SuspendLayout();
-        	// 
-        	// webBrowser1
-        	// 
-        	this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-			| System.Windows.Forms.AnchorStyles.Left) 
-			| System.Windows.Forms.AnchorStyles.Right)));
-        	this.webBrowser1.Location = new System.Drawing.Point(5, 25);
-        	this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-        	this.webBrowser1.Name = "webBrowser1";
-        	this.webBrowser1.Size = new System.Drawing.Size(657, 413);
-        	this.webBrowser1.TabIndex = 0;
-        	// 
-        	// textBox1
-        	// 
-        	this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-			| System.Windows.Forms.AnchorStyles.Right)));
-        	this.textBox1.Location = new System.Drawing.Point(3, 2);
-        	this.textBox1.Name = "textBox1";
-        	this.textBox1.ReadOnly = true;
-        	this.textBox1.Size = new System.Drawing.Size(658, 20);
-        	this.textBox1.TabIndex = 1;
-        	// 
-        	// button1
-        	// 
-        	this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-        	this.button1.Location = new System.Drawing.Point(586, 444);
-        	this.button1.Name = "button1";
-        	this.button1.Size = new System.Drawing.Size(75, 23);
-        	this.button1.TabIndex = 2;
-        	this.button1.Text = "Hide";
-        	this.button1.UseVisualStyleBackColor = true;        	
-        	// 
-        	// WebPanel
-        	// 
-        	this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-        	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        	this.Controls.Add(this.button1);
-        	this.Controls.Add(this.textBox1);
-        	this.Controls.Add(this.webBrowser1);
-        	this.Name = "WebPanel";
-        	this.Size = new System.Drawing.Size(664, 475);
-        	this.ResumeLayout(false);
-        	this.PerformLayout();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(5, 25);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(657, 413);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(658, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(586, 444);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Hide";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // WebPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "WebPanel";
+            this.Size = new System.Drawing.Size(664, 475);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
     }
