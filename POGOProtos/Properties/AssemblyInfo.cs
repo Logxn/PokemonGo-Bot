f55abf6037f0843f9f7bf2a829cef122b5a4a030
Logan <logxncs@gmail.com>
2017-04-02 20:33:58 +0200

    Updated protos to 0.59.1 (or 2?) xd

--- POGOProtos/Properties/AssemblyInfo.cs
+++ POGOProtos/Properties/AssemblyInfo.cs
@@ -1,10 +1,14 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿#region using directives
+
+using System.Reflection;
 using System.Runtime.InteropServices;
+
+#endregion
 
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
+
 [assembly: AssemblyTitle("POGOProtos")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -17,9 +21,11 @@ using System.Runtime.InteropServices;
 // Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
 // für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
 // COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+
 [assembly: ComVisible(false)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+
 [assembly: Guid("a03a7bb2-4f0b-467b-84b2-9a76e6aae6fb")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
@@ -32,5 +38,6 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
